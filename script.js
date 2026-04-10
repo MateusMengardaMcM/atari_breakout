1ce2bf48d4b0f6a19744cbb570bd70f36d56c928
Mateus Mengarda <mateus.byjus@gmail.com>
2026-04-10 18:57:30 -0300

    parte da movimentação da bola e da raquete iniciada

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function setup() {
         x: width / 2,
         y: height / 2,
         r: 10,
-        vx: 2,
+        vx: 0,
         vy: 0
     }
 
@@ -29,10 +29,14 @@ function draw() {
     ellipseMode(RADIUS);
     fill("white");
     ellipse(ball.x, ball.y, ball.r);
-    vx 
+    ball.vx = random(-4, 4);
+    ball.vy = 4;
+    ball.x += ball.vx;
+    ball.y += ball.vy;
 
     //raquete
     rectMode(CENTER);
     fill("yellow");
     rect(paddle.x, paddle.y, paddle.w, paddle.h);
+    paddle.x = constrain(mouseX, paddle.w / 2, width - paddle.w / 2);
 }
