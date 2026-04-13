ed6261d82e681a2d4e4f79fcb984351e6cbf6b62
Mateus Mengarda <mateus.byjus@gmail.com>
2026-04-13 18:55:10 -0300

    função mousePressed e os tijolos(obstáculos para a bola) inseridos no código)

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+//variáveis
 let ball;
 let paddle;
+let bricks = [];
+let rows = 4;
+let cols = 8;
+let brickWidth = 60;
+let brickHeight = 20;
+let spacing = 5;
 
 function setup() {
     createCanvas(600, 600);
@@ -20,17 +27,21 @@ function setup() {
         w: 100,
         h: 10
     }
+
+    //chamando a função para a criação dos tijolos
+    createBrics();
 }
 
 function draw() {
+    //fundo
     background(0);
     
     //bola
     ellipseMode(RADIUS);
     fill("white");
     ellipse(ball.x, ball.y, ball.r);
-    ball.vx = random(-4, 4);
-    ball.vy = 4;
+
+    //movimento da bola
     ball.x += ball.vx;
     ball.y += ball.vy;
 
@@ -39,4 +50,50 @@ function draw() {
     fill("yellow");
     rect(paddle.x, paddle.y, paddle.w, paddle.h);
     paddle.x = constrain(mouseX, paddle.w / 2, width - paddle.w / 2);
+
+    //desenho dos tijolos
+    for (let i = 0; i < bricks.length; i++) {
+        fill (bricks[i].color);
+        rect(bricks[i].x, bricks[i].y, bricks[i].w, bricks[i].h);
+    }
+
+    //colisão da bola com a raquete
+    if (ball.y + ball.r > paddle.y - paddle.h / 2 &&
+        ball.y + ball.r < paddle.y + paddle.h / 2 &&
+        ball.x > paddle.x - paddle.w / 2 &&
+        ball.x < paddle.x + paddle.w / 2) {
+        ball.vy *= -1;
+        let diff = ball.x - paddle.x;
+        ball.vx = diff * 0.1;
+    }
+
+    //colisão da bola com os tijolosc
+    
+    //colisão da bola com as paredes
+    if (ball.x - ball.r < 0 || ball.x + ball.r > width) ball.vx *= -1;
+    if (ball.y - ball.r < 0) ball.vy *= -1;
+}
+
+//jogo iniciado ao clicar com o mouse
+function mousePressed() {
+    ball.vx = random(-4, 4);
+    ball.vy = 4;
+}
+
+//criação dos tijolos
+function createBrics() {
+    bricks = [];
+    let totalWidth = cols * (brickWidth + spacing) - spacing;
+    let startX = (width - totalWidth) - 15;
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            bricks.push({
+                x: startX + c * (brickWidth + spacing),
+                y: 80 + r * brickHeight,
+                w: brickWidth - spacing,
+                h: brickHeight - 5,
+                color: [random(100,255), random(100,255), random(100,255)]
+            });
+        }  
+    }
 }
